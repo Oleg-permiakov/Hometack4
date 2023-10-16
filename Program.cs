3f210abb434d9a89cd8e5f47ba5db0a1d94b3090
Oleg <schika145@gmail.com>
2023-10-16 17:39:19 +0500

    Конец

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа которая принимает три числа и выдаёт наибольшее из них
+Console.WriteLine("Введите первое число");
+string namb = Console.ReadLine();
+int namber1 = Convert.ToInt32(namb);
+
+Console.WriteLine("Введите второе число");
+string namb2 = Console.ReadLine();
+int namber2 = Convert.ToInt32(namb2);
+
+Console.WriteLine("Введите третье число");
+string namb3 = Console.ReadLine();
+int namber3 = Convert.ToInt32(namb3);
+
+int result = namber1;
+     if (namber2 > result) result = namber2;
+     if (namber3 > result) result = namber3;
+     Console.WriteLine(result);
